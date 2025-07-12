24965bac577baff6494e6fafbde7834536e9ace7
rosen <rose.night0729@gmail.com>
2025-07-13 02:10:45 +0900

    バリデーション追加（RegisterRequest、LoginRequest）

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -3,6 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
 
 class AuthController extends Controller
 {
@@ -16,10 +22,31 @@ class AuthController extends Controller
         return view('auth.register');
     }
 
-    public function showLoginForm()
+    public function store(RegisterRequest $request)
+    {
+        User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => Hash::make($request->password),
+        ]);
+
+        return redirect('/login');
+    }
+
+        public function loginForm()
     {
         return view('auth.login');
     }
+
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect('/admin');
+        }
+
+    }
+
 }
-
-
