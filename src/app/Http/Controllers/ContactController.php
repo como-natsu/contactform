8d0ca3075c1fe7859dd4c216a91d1ccc4e8a8300
rosen <rose.night0729@gmail.com>
2025-07-15 13:25:55 +0900

    ContactController　useの入力ミス修正

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
-use\App\Models\Contact;
+use App\Models\Contact;
 use App\Http\Requests\ContactRequest;
 
 class ContactController extends Controller
